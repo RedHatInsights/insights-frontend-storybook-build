82ed08f0eaca966cd092d992c1e45527f1b0d8f2
Travis CI User <travis@example.org>
2019-01-16 18:43:35 +0000

    Merge pull request #42 from RedHatInsights/stories/skeleton
    adding story for skeleton

--- config.js
+++ config.js
@@ -1,12 +1,17 @@
 // config.js
 import React from 'react';
+import { Provider } from 'react-redux';
+import { init } from '../src/store';
 import { configure, addDecorator, setAddon } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
 import { withInfo } from '@storybook/addon-info';
+import logger from 'redux-logger';
 
 import chaptersAddon, { setDefaults } from 'react-storybook-addon-chapters';
 
 import { Button } from '@patternfly/react-core';
+
+import '../src/stories.scss';
 
 const api = require.context('../src/stories/api', true, /stories\.js$/);
 const chrome = require.context('../src/stories/chrome', true, /stories\.js$/);
@@ -15,6 +20,7 @@ const redux = require.context('../src/stories/redux', true, /stories\.js$/);
 const repos = require.context('../src/stories/repos', true, /stories\.js$/);
 const welcome = require.context('../src/stories/welcome', true, /stories\.js$/);
 const charts = require.context('../src/stories/charts', true, /stories\.js$/);
+const deployments = require.context('../src/stories/deployments', true, /stories\.js$/);
 
 setDefaults({
   sectionOptions: {
@@ -30,22 +36,25 @@ setAddon(chaptersAddon);
 // addon-options
 setOptions({
   name: 'Red Hat Insights', // Name in top left Corner
-  url: 'https://access.redhat.com/insights/platform/', // URL
+  url: 'https://github.com/RedHatInsights', // URL
   addonPanelInRight: true,
   sortStoriesByKind: false
 });
 
 //adding global decorators
 addDecorator(story => (
-  <div style={{padding: '15px'}}>
-    {story()}
-  </div>
+  <Provider store={ init().getStore() }>
+    <div style={{padding: '15px'}}>
+      {story()}
+    </div>
+  </Provider>
 ));
 
 function loadStories() {
   welcome.keys().forEach(welcome);
   repos.keys().forEach(repos);
   chrome.keys().forEach(chrome);
+  deployments.keys().forEach(deployments);
   api.keys().forEach(api);
   redux.keys().forEach(redux);
   components.keys().forEach(components);
