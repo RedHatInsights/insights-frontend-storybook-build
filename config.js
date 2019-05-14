f1a6a505f1b66f7b89ab8660628bc08038c9464f
Travis CI User <travis@example.org>
2019-05-14 13:17:48 +0000

    adding custom release

--- config.js
+++ config.js
@@ -1,20 +1,22 @@
 // config.js
 import React from 'react';
+import { Provider } from 'react-redux';
+import { init } from '../src/store';
 import { configure, addDecorator, setAddon } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
-import { withInfo } from '@storybook/addon-info';
 
 import chaptersAddon, { setDefaults } from 'react-storybook-addon-chapters';
 
-import { Button } from '@patternfly/react-core';
+import '../src/stories.scss';
 
-const api = require.context('../src/stories/api', true, /stories\.js$/);
 const chrome = require.context('../src/stories/chrome', true, /stories\.js$/);
 const components = require.context('../src/stories/components', true, /stories\.js$/);
 const redux = require.context('../src/stories/redux', true, /stories\.js$/);
 const repos = require.context('../src/stories/repos', true, /stories\.js$/);
 const welcome = require.context('../src/stories/welcome', true, /stories\.js$/);
 const charts = require.context('../src/stories/charts', true, /stories\.js$/);
+const deployments = require.context('../src/stories/deployments', true, /stories\.js$/);
+const uxd = require.context('../src/stories/uxd', true, /stories\.js$/);
 
 setDefaults({
   sectionOptions: {
@@ -29,27 +31,30 @@ setAddon(chaptersAddon);
 
 // addon-options
 setOptions({
-  name: 'Red Hat Insights', // Name in top left Corner
-  url: 'https://access.redhat.com/insights/platform/', // URL
+  name: 'cloud.redhat.com', // Name in top left Corner
+  url: 'https://github.com/RedHatInsights', // URL
   addonPanelInRight: true,
   sortStoriesByKind: false
 });
 
 //adding global decorators
 addDecorator(story => (
-  <div style={{padding: '15px'}}>
-    {story()}
-  </div>
+  <Provider store={ init().getStore() }>
+    <div style={{padding: '15px'}}>
+      {story()}
+    </div>
+  </Provider>
 ));
 
 function loadStories() {
   welcome.keys().forEach(welcome);
   repos.keys().forEach(repos);
   chrome.keys().forEach(chrome);
-  api.keys().forEach(api);
+  deployments.keys().forEach(deployments);
   redux.keys().forEach(redux);
   components.keys().forEach(components);
   charts.keys().forEach(charts);
+  uxd.keys().forEach(uxd);
 }
 
 configure(loadStories, module);
